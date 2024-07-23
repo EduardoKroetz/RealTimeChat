0cde9201c592ad19b2667afe03b300ac78e03cb2
Eduardo Kroetz <eduardokroetz0@gmail.com>
2024-07-23 14:17:37 -0300

    feature: create auth service

---
+++ RealTimeChat.Core/Services/IAuthService.cs
@@ -0,0 +1,7 @@
+﻿namespace RealTimeChat.Core.Services;
+
+public interface IAuthService
+{
+    string GenerateJwtToken(string email, string role);
+    string HashPassword(string password);
+}
---
+++ RealTimeChat.Infrastructure/Auth/AuthService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using RealTimeChat.Core.Services;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace RealTimeChat.Infrastructure.Auth;
+
+public class AuthService : IAuthService
+{
+    private readonly IConfiguration _configuration;
+
+    public AuthService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string HashPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public string GenerateJwtToken(string email, string role)
+    {
+        var key = _configuration["JwtKey"]!;
+
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Email, email),
+            new Claim(ClaimTypes.Role, role)
+        };
+
+        var token = new JwtSecurityToken
+        (
+            expires: DateTime.Now.AddHours(8),
+            signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)), SecurityAlgorithms.HmacSha256),
+            claims: claims
+        );
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var stringToken = tokenHandler.WriteToken(token);
+
+        return stringToken;
+    }
+}
