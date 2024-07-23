ad0751cfa5cadf9abf08f7c7c0e35b28c28abb4e
Eduardo Kroetz <eduardokroetz0@gmail.com>
2024-07-23 14:25:17 -0300

    feature: add MediatR to project, inject dependencies, create auth controller and register user endpoint

---
+++ RealTimeChat.API/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using RealTimeChat.Application.Commands.CreateUser;
+
+namespace RealTimeChat.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public AuthController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> RegisterAsync([FromBody] CreateUserCommand command)
+    {
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+
+}
--- RealTimeChat.API/Program.cs
+++ RealTimeChat.API/Program.cs
@@ -2,13 +2,21 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using RealTimeChat.API;
+using RealTimeChat.Infrastructure.Persistence.Context;
+using RealTimeChat.Application.Commands.CreateUser;
 using System.Text;
+using RealTimeChat.Core.Services;
+using RealTimeChat.Infrastructure.Auth;
+using RealTimeChat.Core.Repositories;
+using RealTimeChat.Infrastructure.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMediatR(options => options.RegisterServicesFromAssembly(typeof(CreateUserCommand).Assembly));
 
 LoadConfiguration(builder.Configuration);
 ConfigureServices(builder.Services);
@@ -44,6 +52,12 @@ void ConfigureServices(IServiceCollection services)
         opt.UseSqlServer(Configuration.ConnectionString);
     });
 
+    //Injetando dependências
+    services.AddScoped<IAuthService, AuthService>();
+    services.AddScoped<IUserRepository, UserRepository>();
+    services.AddScoped<IChatRoomRepository, ChatRoomRepository>();
+    services.AddScoped<IMessageRepository, MessageRepository>();
+    services.AddScoped<IRoomParticipantRepository, RoomParticipantRepository>();
 
     var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
     services.AddAuthentication(x =>
