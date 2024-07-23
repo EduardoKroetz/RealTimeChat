27f292a5e5a3f7c776d64a89ba364e2fe003711f
Eduardo Kroetz <eduardokroetz0@gmail.com>
2024-07-23 14:26:32 -0300

    feature: delete .Data project, create Result DTO and Dockerfile

--- RealTimeChat.Core/Repositories/IUserRepository.cs
+++ RealTimeChat.Core/Repositories/IUserRepository.cs
@@ -6,6 +6,7 @@ namespace RealTimeChat.Core.Repositories;
 public interface IUserRepository
 {
     Task<User?> GetByIdAsync(Guid userId);
+    Task<User?> GetUserByEmailAsync(string email);
     Task AddAsync(User user);
     Task UpdateAsync(User user);
     Task DeleteAsync(User user);
