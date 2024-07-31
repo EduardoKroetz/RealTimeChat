9aa567024be5fcabd4acf8748dee3133db79f133
Eduardo Kroetz <eduardokroetz0@gmail.com>
2024-07-31 14:31:36 -0300

    feature: create SearchRooms component and add user icon to header to view user options

--- RealTimeChat.Site/src/Components/Header/index.tsx
+++ RealTimeChat.Site/src/Components/Header/index.tsx
@@ -1,13 +1,26 @@
 import { Link } from "react-router-dom"
 import "./style.css"
+import { useContext } from "react"
+import { ScreenWidthContext } from "../../Contexts/ScreenWidthContext"
+import SearchRooms from "../SearchRooms";
 
 export default function Header()
 {
+  const screenWidth = useContext(ScreenWidthContext);
+
   return (
     <div className="header-container">
       <Link to={"/"}>
         <h1 className="header-title">RealTimeChat</h1>
       </Link>
+      <div className="header-content">
+        {screenWidth > 425 && 
+          <SearchRooms />
+        }
+        <div className="header-user-info">
+          <i title="Você" className="fas fa-user user-icon"></i>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ RealTimeChat.Site/src/Components/SearchRooms/index.tsx
@@ -0,0 +1,11 @@
+import "./style.css"
+
+export default function SearchRooms()
+{
+  return (
+    <div className="search-rooms-container">
+      <input type="text" placeholder="Search rooms" className="search-rooms-input"/>
+    </div>
+
+  )
+}
