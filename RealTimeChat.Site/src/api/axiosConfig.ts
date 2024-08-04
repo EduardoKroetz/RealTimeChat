ceaf226691dfb3ee64330a84c08138387ded9990
Eduardo Kroetz <eduardokroetz0@gmail.com>
2024-08-04 17:29:14 -0300

    adjust environment variables

--- RealTimeChat.Site/src/api/axiosConfig.ts
+++ RealTimeChat.Site/src/api/axiosConfig.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-export const baseUrl = process.env.BACKEND_URL; 
-console.log(baseUrl)
+export const baseUrl = import.meta.env.VITE_BACKEND_URL; //coloque um arquivo .env com a url do backend: VITE_BACKEND_URL=url do backend
 
 const api = axios.create({
   baseURL: `${baseUrl}/api`,
